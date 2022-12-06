d2a23dc23cf76bcdb40d9651ae71c14ffb480475
dg0397 <rgg0397@gmail.com>
2022-12-05 23:43:36 -0400

    Adding challenge 05's solution

---
+++ 05/index.js
@@ -0,0 +1,17 @@
+function getMaxGifts(giftsCities, maxGifts, maxCities) {
+  return Math.max(
+    ...giftsCities
+      .reduce(
+        (acc, giftsCities) =>
+          acc.concat(acc.map((e) => [giftsCities].concat(e))),
+        [[]]
+      )
+      .filter(
+        (solution) =>
+          solution.length <= maxCities &&
+          solution.reduce((acc, gifts) => (acc += gifts), 0) <= maxGifts &&
+          true
+      )
+      .map((solution) => solution.reduce((acc, b) => acc + b, 0))
+  );
+}

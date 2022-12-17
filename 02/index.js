d79ab5a6a4cb8c653af46b3f5cfe217af345eccd
dg0397 <rgg0397@gmail.com>
2022-12-16 20:23:20 -0400

    Improving challenge02's solution

--- 02/index.js
+++ 02/index.js
@@ -4,3 +4,21 @@ function countHours(year, holidays) {
     return day !== 0 && day !== 6 ? (acc += 2) : acc;
   }, 0);
 }
+/*
+function countHours(year, holidays) {
+  return holidays
+    .reduce((acc, holiday) => {
+      [1, 2, 3, 4, 5].includes(new Date(holiday + "-" + year).getDay()) &&
+        acc.push(2);
+      return acc;
+    }, [])
+    .reduce((acc, b) => acc + b, 0);
+}
+
+function countHours(year, holidays) {
+  return (
+    holidays.filter((holiday) => new Date(holiday + "/" + year).getDay() % 6)
+      .length * 2
+  );
+}
+*/
